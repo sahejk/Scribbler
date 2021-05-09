cb817e363469dec6933341f1fc9abe7680fc9172
Sahej Kaur Makkar <sahejk@fb.com>
2021-05-09 15:20:10 +0800

    Fix issue in css, styling

--- scripts/postslist.js
+++ scripts/postslist.js
@@ -1,16 +1,11 @@
 function removeCard(card) {
-        let delCard = document.getElementById(card);
-
+        const delCard = document.getElementById(card);
         delCard.parentNode.removeChild(delCard);
 }
 
 function openPost(author, heading, content) {
-        var url =
-                '../html/post.html?heading=' +
-                encodeURIComponent(heading.innerText) +
-                '&author=' +
-                encodeURIComponent(author.innerText) +
-                '&content=' +
-                encodeURIComponent(content.innerText);
+        const url = `../html/post.html?heading=${encodeURIComponent(heading.innerText)}&author=${encodeURIComponent(
+                author.innerText
+        )}&content=${encodeURIComponent(content.innerText)}`;
         window.location.href = url;
 }
